d413fde08a23ef410113af62eedfefe05261faea
ryanarjun <karshan813@gmail.com>
2018-06-02 12:07:08 -0400

    Added eslint, bcrypt and mire

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import auth from './routes/auth';
+import bodyParser from 'body-parser';
 
 const app = express();
+const uri = "mongodb://mongo:27017/bookapp";
+const options = {
+    useMongoClient: true,
+    // autoIndex: false, // Don't build indexes
+    // reconnectTries: 100, // Never stop trying to reconnect
+    // reconnectInterval: 500, // Reconnect every 500ms
+    // poolSize: 10, // Maintain up to 10 socket connections
+    // // If not connected, return errors immediately rather than waiting for reconnect
+    // bufferMaxEntries: 0
+  };
+app.use(bodyParser.json());
+mongoose.connect(uri).catch(err => {})
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: {global: "Invalid Credentials"}});
-})
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
