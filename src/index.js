959f4602ac2780255d51c9409cdf9aa8e2f4e5cb
ryanarjun <karshan813@gmail.com>
2018-06-08 17:34:14 -0400

    Added JWT for user to have a token upon access to the website

--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@ import path from 'path';
 import mongoose from 'mongoose';
 import auth from './routes/auth';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 const app = express();
-const uri = "mongodb://mongo:27017/bookapp";
-const options = {
-    useMongoClient: true,
-    // autoIndex: false, // Don't build indexes
-    // reconnectTries: 100, // Never stop trying to reconnect
-    // reconnectInterval: 500, // Reconnect every 500ms
-    // poolSize: 10, // Maintain up to 10 socket connections
-    // // If not connected, return errors immediately rather than waiting for reconnect
-    // bufferMaxEntries: 0
-  };
+const uri = process.env.MONGODB_URL;
 app.use(bodyParser.json());
+
 mongoose.connect(uri).catch(err => {})
 
 app.use('/api/auth', auth);
@@ -24,4 +19,4 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 })
 
-app.listen(8080, () => console.log('runing on 8080'));
+app.listen(8080, () => console.log('running on 8080'));
